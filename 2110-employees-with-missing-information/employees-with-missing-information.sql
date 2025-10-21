01a4723fc7b5194ac3ab77eae5540dd92fc61d6b
Swarn Suman <swarnsuman004@gmail.com>
2025-10-21 10:56:12 +0530

    Time: 708 ms (12.28%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -3,11 +3,11 @@ SELECT E.employee_id
 FROM Employees E
 LEFT JOIN Salaries S
 ON E.employee_id = S.employee_id
-WHERE E.name IS NULL OR S.salary IS NULL
+WHERE S.salary IS NULL
 UNION
 SELECT S.employee_id
 FROM Employees E
 RIGHT JOIN Salaries S
 ON E.employee_id = S.employee_id
-WHERE E.name IS NULL OR S.salary IS NULL
+WHERE E.name IS NULL
 ORDER BY employee_id;
