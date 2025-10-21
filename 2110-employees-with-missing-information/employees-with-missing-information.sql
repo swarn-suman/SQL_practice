21c01cc886676e691638baf06342a8f468413340
Swarn Suman <swarnsuman004@gmail.com>
2025-10-21 10:53:20 +0530

    Time: 537 ms (56.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT E.employee_id
+FROM Employees E
+LEFT JOIN Salaries S
+ON E.employee_id = S.employee_id
+WHERE E.name IS NULL OR S.salary IS NULL
+UNION
+SELECT S.employee_id
+FROM Employees E
+RIGHT JOIN Salaries S
+ON E.employee_id = S.employee_id
+WHERE E.name IS NULL OR S.salary IS NULL
+ORDER BY employee_id;
