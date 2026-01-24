a101383ffb038bf653c2a10168d5b7dfc065b721
Swarn Suman <swarnsuman004@gmail.com>
2026-01-24 20:10:35 +0530

    Time: 564 ms (39.08%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,11 +1,11 @@
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
-FROM Insurance
-WHERE tiv_2015 IN (
-    SELECT tiv_2015
+SELECT 
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM (
+    SELECT 
+        tiv_2016,
+        COUNT(*) OVER (PARTITION BY tiv_2015) AS same_tiv_2015_cnt,
+        COUNT(*) OVER (PARTITION BY lat, lon) AS same_location_cnt
     FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*) > 1) AND (lat, lon) IN (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1);
+) t
+WHERE same_tiv_2015_cnt > 1
+  AND same_location_cnt = 1;
