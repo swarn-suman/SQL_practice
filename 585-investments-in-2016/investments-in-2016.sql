1e4fbbb9fcf72f801474c6f558109af2ec73ab19
Swarn Suman <swarnsuman004@gmail.com>
2026-01-24 19:41:02 +0530

    Time: 496 ms (90.70%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -4,10 +4,8 @@ WHERE tiv_2015 IN (
     SELECT tiv_2015
     FROM Insurance
     GROUP BY tiv_2015
-    HAVING COUNT(*) > 1)
-AND (lat, lon) IN (
+    HAVING COUNT(*) > 1) AND (lat, lon) IN (
     SELECT lat, lon
     FROM Insurance
     GROUP BY lat, lon
     HAVING COUNT(*) = 1);
-
