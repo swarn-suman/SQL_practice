f449e3b2e835dcc2f39cffb15623cf8e769c9ae9
Swarn Suman <swarnsuman004@gmail.com>
2026-01-29 23:23:53 +0530

    Time: 525 ms (21.02%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
-FROM Activity
-GROUP BY activity_date
-HAVING MIN(activity_date) > '2019-06-27' AND MAX(activity_date) <= '2019-07-27';
+    SELECT  activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+    FROM Activity
+    GROUP BY activity_date
+    HAVING DATEDIFF('2019-07-27', activity_date) >= 0 AND DATEDIFF('2019-07-27', activity_date) < 30
