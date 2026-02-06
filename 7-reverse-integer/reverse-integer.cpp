65f093d97a7bd14d08b49d270b372d99c725e191
Swarn Suman <swarnsuman004@gmail.com>
2026-02-07 02:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.96%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int duplicate_x = x;
-        int rev = 0;
+  int reverse(int x) {
+    long long n = x;
+    long long rev = 0;
 
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-
-            // overflow check before multiplying
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) {
-                return 0;
-            }
-
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) {
-                return 0;
-            }
-
-            rev = rev * 10 + digit;
-        }
-
-        return rev;
+    while (n != 0) {
+      int last = n % 10;
+      rev = rev * 10 + last;
+      n /= 10;
     }
+
+    if (rev > INT_MAX || rev < INT_MIN){
+        return 0;
+    }
+    return (int)rev;
+  }
 };
