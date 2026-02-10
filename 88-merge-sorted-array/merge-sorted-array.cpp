df38afa50495b5f11d2bff3d6317104d3710c35d
Swarn Suman <swarnsuman004@gmail.com>
2026-02-10 19:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.02%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> nums1Copy(nums1.begin(), nums1.begin() + m);
+
+          int p1 = 0, p2 = 0;
+          for (int i = 0; i < m + n; i++) {
+              if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+                  nums1[i] = nums1Copy[p1];
+                  p1++;
+              } 
+              else {
+                  nums1[i] = nums2[p2];
+                  p2++;
+              }
+          }
+    }    
+};
