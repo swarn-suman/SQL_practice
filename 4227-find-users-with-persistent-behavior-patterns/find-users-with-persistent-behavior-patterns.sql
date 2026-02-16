68bdb71c330e03d68a32f9591a217a2ea04cb060
Swarn Suman <swarnsuman004@gmail.com>
2026-02-16 17:03:12 +0530

    Time: 290 ms (63.32%) | Memory: 0B (100.00%) - LeetSync

--- 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
+++ 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
@@ -1,8 +1,47 @@
 # Write your MySQL query statement below
 
-    SELECT user_id, action, COUNT(action_date) AS streak_length,
-    MIN(action_date) AS start_date, MAX(action_date) AS end_date
+WITH numbered AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY user_id, action
+               ORDER BY action_date
+           ) AS rn
     FROM activity
-    GROUP BY user_id, action
-    HAVING streak_length >= 5
-    ORDER BY streak_length DESC, user_id ASC
+),
+
+grouped AS (
+    SELECT user_id,
+           action,
+           action_date,
+           DATE_SUB(action_date, INTERVAL rn DAY) AS grp
+    FROM numbered
+),
+
+streaks AS (
+    SELECT user_id,
+           action,
+           COUNT(*) AS streak_length,
+           MIN(action_date) AS start_date,
+           MAX(action_date) AS end_date
+    FROM grouped
+    GROUP BY user_id, action, grp
+    HAVING COUNT(*) >= 5
+),
+
+max_streak AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY user_id
+               ORDER BY streak_length DESC
+           ) AS rnk
+    FROM streaks
+)
+
+SELECT user_id,
+       action,
+       streak_length,
+       start_date,
+       end_date
+FROM max_streak
+WHERE rnk = 1
+ORDER BY streak_length DESC, user_id ASC;
