0a52007fef7ea071cd92a504cfc0506e217d8d3d
Swarn Suman <swarnsuman004@gmail.com>
2026-02-25 01:56:22 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.06%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;   
+
+        while(left <= right){
+            int middle = (left + right)/2;
+
+            if(target == nums[middle]){
+                return middle;
+            }
+
+            else if(target < nums[middle]){
+                right = middle-1;
+            }
+
+            else{
+                left = middle+1;
+            }
+        }
+        return -1;
+    }
+};
