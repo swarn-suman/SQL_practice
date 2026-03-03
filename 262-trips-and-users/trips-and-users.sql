7de4b66d83256d985b0076f3709ab40583128bdb
Swarn Suman <swarnsuman004@gmail.com>
2026-03-03 17:36:28 +0530

    Time: 490 ms (93.47%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,22 +1,21 @@
-SELECT DISTINCT request_at AS Day, ROUND(cancelled_cnt / total_cnt, 2) AS `Cancellation Rate`
-FROM (
-    SELECT
-        T.request_at,
-        COUNT(*) OVER (PARTITION BY T.request_at) AS total_cnt,
-        SUM(
-            CASE
-                WHEN T.status IN ('cancelled_by_client', 'cancelled_by_driver') THEN 1
-                ELSE 0
-            END
-        ) OVER (PARTITION BY T.request_at) AS cancelled_cnt
+# Write your MySQL query statement below
+WITH temp AS(
+    SELECT DISTINCT T.request_at AS `Day`,
+    COUNT(*) OVER(PARTITION BY T.request_at) AS total_count,
+    SUM(
+        CASE WHEN T.status IN('cancelled_by_driver','cancelled_by_client') THEN 1
+        ELSE 0 END) OVER(PARTITION BY T.request_at) AS cancelled_count
     FROM Trips T
     JOIN Users C
-        ON T.client_id = C.users_id
-        AND C.banned = 'No'
-        AND C.role = 'client'
+    ON T.client_id = C.users_id
+    AND C.banned = 'No'
+    AND C.role = 'client'
     JOIN Users D
-        ON T.driver_id = D.users_id
-        AND D.banned = 'No'
-        AND D.role = 'driver'
-    WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-) temp;
+    ON T.driver_id = D.users_id
+    AND D.banned = 'No'
+    AND D.role = 'driver'
+    WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03' 
+)
+
+SELECT `Day`, ROUND(cancelled_count/total_count,2) AS `Cancellation Rate`
+FROM temp
