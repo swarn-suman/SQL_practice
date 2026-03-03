1442225e72904141688ba776a0511bbe3f2e62a4
Swarn Suman <swarnsuman004@gmail.com>
2026-03-03 20:07:15 +0530

    Time: 507 ms (90.34%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,11 +1,12 @@
-SELECT 
-    ROUND(COUNT(DISTINCT a.player_id) /(SELECT COUNT(DISTINCT player_id) FROM Activity),2) 
-    AS fraction
-FROM Activity a
-JOIN (
-    SELECT player_id, MIN(event_date) AS first_login
+WITH first_login AS (
+    SELECT player_id,MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
-) f
-ON a.player_id = f.player_id
-AND DATEDIFF(a.event_date, f.first_login) = 1;
+)
+
+SELECT 
+ROUND(COUNT(DISTINCT f.player_id) /(SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM first_login f
+JOIN Activity a
+    ON f.player_id = a.player_id
+   AND DATEDIFF(a.event_date,f.first_date) = 1
