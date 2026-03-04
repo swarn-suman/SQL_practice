d123c2e70f372174f99db1910202f9db7977ca1d
Swarn Suman <swarnsuman004@gmail.com>
2026-03-04 17:25:19 +0530

    Time: 840 ms (96.22%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT E.name, B.Bonus
+SELECT E.name, B.bonus
 FROM Employee E
 LEFT JOIN Bonus B
 ON E.empId = B.empId
-WHERE B.bonus < 1000 OR B.bonus IS NULL;
+WHERE B.bonus < 1000 OR B.bonus IS NULL
