b5fb41e555158bb46dbcb7ca148116344475bf62
Swarn Suman <swarnsuman004@gmail.com>
2026-03-11 23:58:30 +0530

    Time: 535 ms (5.71%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,14 @@
-# Write your MySQL query statement below
+WITH temp AS(
+    SELECT S1.id AS id1, S1.student AS student, S2.id AS id2
+    FROM Seat S1
+    LEFT JOIN Seat S2
+    ON S2.id = S1.id + 1
+)
+
 SELECT CASE
-        WHEN id % 2 = 1 THEN LEAD(id, 1, id) OVER (ORDER BY id)
-        ELSE LAG(id) OVER (ORDER BY id)
-END AS id, 
-student FROM Seat
-ORDER BY id;
+WHEN id1 % 2 = 1 AND id1 NOT IN(SELECT MAX(id) FROM Seat) THEN id2
+WHEN id1 % 2 = 1 AND id1 IN(SELECT MAX(id) FROM Seat) THEN id1
+ELSE id1 - 1 END AS id,
+student FROM temp
+ORDER BY id
 
