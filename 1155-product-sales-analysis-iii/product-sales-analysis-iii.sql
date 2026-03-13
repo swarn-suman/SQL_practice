371224097061266af8aeef58bf57cb16abbbbd94
Swarn Suman <swarnsuman004@gmail.com>
2026-03-13 20:08:36 +0530

    Time: 737 ms (39.74%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-SELECT product_id, year AS first_year, quantity, price
-FROM(
-    SELECT product_id, year, quantity, price,
-    DENSE_RANK() OVER(PARTITION BY product_id ORDER BY year) AS position
+WITH first AS(
+    SELECT *, 
+    DENSE_RANK() OVER(PARTITION BY product_id ORDER BY year) AS rnk
     FROM Sales
-)t
-WHERE position = 1;
+)
+
+SELECT product_id, year AS first_year, quantity, price
+FROM first 
+WHERE rnk = 1
