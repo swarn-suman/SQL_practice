6564d0e77d79d4b2822fb3244f50b59b1b8de8d6
Swarn Suman <swarnsuman004@gmail.com>
2026-03-17 16:00:34 +0530

    Time: 485 ms (48.32%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,17 +1,12 @@
 # Write your MySQL query statement below
-SELECT 
-    p.product_id,
-    COALESCE(
-        (
-            SELECT new_price
-            FROM Products p2
-            WHERE p2.product_id = p.product_id
-              AND p2.change_date <= '2019-08-16'
-            ORDER BY p2.change_date DESC
-            LIMIT 1
-        ),
-        10
-    ) AS price
-FROM (SELECT DISTINCT product_id FROM Products) p;
+WITH ranked AS (
+    SELECT product_id, new_price, change_date,
+        DENSE_RANK() OVER (PARTITION BY product_id ORDER BY change_date DESC) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
 
-
+SELECT P.product_id, COALESCE(R.new_price, 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) P
+LEFT JOIN ranked R
+ON P.product_id = R.product_id AND R.rn = 1;
