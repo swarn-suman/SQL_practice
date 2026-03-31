22c8f686c4f7799a4da912b2a0001caec2a121b5
Swarn Suman <swarnsuman004@gmail.com>
2026-03-31 18:37:46 +0530

    Time: 238 ms (59.67%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,11 +1,24 @@
 # Write your MySQL query statement below
-SELECT machine_id, ROUND(AVG(end_time - start_time), 3) AS processing_time
-FROM (SELECT 
-        machine_id,
-        process_id,
-        MAX(CASE WHEN activity_type = 'start' THEN timestamp END) AS start_time,
-        MAX(CASE WHEN activity_type = 'end' THEN timestamp END) AS end_time
+WITH temp AS(
+    SELECT machine_id, process_id,
+    CASE WHEN activity_type = 'end' THEN timestamp
+    ELSE -timestamp END AS timestamp
     FROM Activity
-    GROUP BY machine_id, process_id
-) AS process_times
-GROUP BY machine_id;
+),
+
+temp1 AS (
+    SELECT machine_id, SUM(timestamp) AS total_time
+    FROM temp
+    GROUP BY machine_id
+),
+
+temp2 AS(
+    SELECT machine_id, COUNT(DISTINCT process_id) AS cnt
+    FROM Activity
+    GROUP BY machine_id
+)
+
+SELECT temp1.machine_id, ROUND(temp1.total_time / temp2.cnt, 3) AS processing_time
+FROM temp1
+JOIN temp2
+ON temp1.machine_id = temp2.machine_id;
