17030efdfbdcb57e8648991a34ddf1c28bc65a9a
Swarn Suman <swarnsuman004@gmail.com>
2026-04-05 22:08:09 +0530

    Time: 573 ms (57.33%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-SELECT employee_id, 
-CASE
-    WHEN employee_id%2 != 0 AND name NOT LIKE'M%' THEN salary
-    ELSE 0
-END AS bonus
+SELECT employee_id,
+CASE WHEN employee_id % 2 = 1 AND SUBSTRING(name,1,1) != 'M' THEN salary
+ELSE 0 END AS bonus
 FROM Employees
-ORDER BY employee_id;
+ORDER BY employee_id
