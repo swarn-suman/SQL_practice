c188bbf29a6b373a54512859d2248d1ec5eff2ca
Swarn Suman <swarnsuman004@gmail.com>
2026-04-06 23:11:26 +0530

    Time: 687 ms (21.81%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-SELECT user_id, MAX(time_stamp) AS last_stamp
-FROM Logins
-WHERE YEAR(time_stamp) = 2020
-GROUP BY user_id;
+WITH temp AS(
+    SELECT *, 
+    DENSE_RANK() OVER(PARTITION BY user_id ORDER BY time_stamp DESC) AS rnk
+    FROM Logins
+    WHERE YEAR(time_stamp) = '2020'
+)
+
+SELECT user_id, time_stamp AS last_stamp
+FROM temp 
+WHERE rnk = 1
