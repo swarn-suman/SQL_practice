c4147433e79ec8096f36a0eda72b2e6ee92162b6
Swarn Suman <swarnsuman004@gmail.com>
2026-04-11 00:31:33 +0530

    Time: 305 ms (32.18%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
-SELECT DISTINCT student_id, subject, first_score, latest_score 
-FROM (
-    SELECT *,FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS first_score,
-           FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score
+WITH temp AS(
+    SELECT *,
+    FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS first_score,
+    FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score,
+    COUNT(*) OVER (PARTITION BY student_id, subject) AS exam_count
     FROM Scores
-)t
-WHERE latest_score > first_score
-ORDER BY student_id, subject;
+)
+
+
+SELECT DISTINCT student_id, subject, first_score, latest_score
+FROM temp 
+WHERE latest_score > first_score AND exam_count >= 2
+ORDER BY student_id, subject
